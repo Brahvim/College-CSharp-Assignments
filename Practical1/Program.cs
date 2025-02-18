d57eff782126a4614eeacaea8ee0931577851a2a
Brahvim <69293652+Brahvim@users.noreply.github.com>
2025-02-18 16:26:21 +0530

    Practical done [and improved]!

--- Practical1/Program.cs
+++ Practical1/Program.cs
@@ -1,16 +1,6 @@
-namespace Practical1
-{
-	class Program
-	{
-		public static void Main(string[] _)
-		{
-			Console.Write("Please enter a character to check the case of: ");
-			Console.WriteLine(
-				char.IsUpper((char)Console.Read())
-				? "That is an uppercase character."
-				: "That is NOT an uppercase character!"
-				);
-		}
-	}
-}
-
+Console.Write("Please enter a character to check the case of: ");
+Console.WriteLine(
+	char.IsUpper((char)Console.Read())
+	? "That is an uppercase character."
+	: "That is NOT an uppercase character!"
+	);

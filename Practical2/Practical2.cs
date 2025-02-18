9bd76528424f4950fd996f46b51106e42a9559d5
Brahvim <69293652+Brahvim@users.noreply.github.com>
2025-02-18 20:41:47 +0530

    Apparently THIS generates less MSIL!

--- Practical2/Practical2.cs
+++ Practical2/Practical2.cs
@@ -1,14 +1,11 @@
 ﻿Console.Write("Please enter a character to check if it is a vowel: ");
 
-string output;
 switch ((char)Console.Read())
 {
 	case 'a':
 	case 'e':
 	case 'i':
 	case 'o':
-	case 'u': { output = "That is a vowel."; } break;
-	default: { output = "That is NOT a vowel!"; } break;
+	case 'u': Console.WriteLine("That is a vowel."); break;
+	default: Console.WriteLine("That is NOT a vowel!"); break;
 }
-
-Console.WriteLine(output);

210d6983cffd1fc8f7a738d28825c0a35be42366
Brahvim <69293652+Brahvim@users.noreply.github.com>
2025-02-18 16:26:06 +0530

    Practical 1 done, improving...

--- Practical1/Program.cs
+++ Practical1/Program.cs
@@ -2,9 +2,14 @@ namespace Practical1
 {
 	class Program
 	{
-		public static void Main(string[] args)
+		public static void Main(string[] _)
 		{
-			Console.WriteLine("Hello, world!");
+			Console.Write("Please enter a character to check the case of: ");
+			Console.WriteLine(
+				char.IsUpper((char)Console.Read())
+				? "That is an uppercase character."
+				: "That is NOT an uppercase character!"
+				);
 		}
 	}
 }

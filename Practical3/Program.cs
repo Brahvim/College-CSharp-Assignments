e302a4e5e3ec401b8dd68eb9cc36a521cddf78eb
brahvim <brahvim@DESKTOP-615JHDG>
2025-03-22 13:26:04 +0530

    Completed Practical 3 in Visual Studio.

---
+++ Practical3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Please enter a string of characters:");
+string uin = Console.ReadLine() ?? "", result = "";
+
+foreach (char c in uin)
+{
+    result += char.IsLower(c)
+        ? char.ToUpperInvariant(c)
+        : char.ToLowerInvariant(c);
+}
+
+Console.WriteLine(result);

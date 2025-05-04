4eb405f79e9f8b723dd3442bc41ef7899599df5b
brahvim <brahvim@DESKTOP-615JHDG>
2025-05-04 17:21:04 +0530

    MORE compact! I'm funny!

--- Practical8/Program.cs
+++ Practical8/Program.cs
@@ -1,19 +1,12 @@
 ﻿// VERY compact program!
 using MySql.Data.MySqlClient;
-string pass = "";
 try
 {
-    pass = File.ReadAllText(".\\password.txt");
-    Console.WriteLine($"`password.txt` said, `{pass}`!");
+    string pass = File.ReadAllText(".\\password.txt"); Console.WriteLine($"`password.txt` said, `{pass}`!");
+    var connection = new MySqlConnection($"User ID=root;Server=localhost;Password={pass};"); // `new()` is lovely but `var` is great, too!:
+    connection.Open(); Console.WriteLine($"Connection status: `{connection.State}`.");
+    int rows = new MySqlCommand(File.ReadAllText(".\\Sql.sql"), connection).ExecuteNonQuery();
+    Console.WriteLine($"{rows} rows affected by all transactions done by the loaded script.");
+    connection.Close(); // Saved *a lot of lines* not using `using() {}`!
 }
-catch (FileNotFoundException)
-{
-    Console.WriteLine("Please make a `password.txt`!");
-}
-// `new()` is lovely but `var` is great, too!:
-var connection = new MySqlConnection($"User ID=root;Server=localhost;Password={pass};");
-connection.Open();
-Console.WriteLine($"Connection status: `{connection.State}`.");
-int rows = new MySqlCommand(File.ReadAllText(".\\Sql.sql"), connection).ExecuteNonQuery();
-Console.WriteLine($"{rows} rows affected by all transactions done by the loaded script.");
-connection.Close(); // Saved *a lot of lines* not using `using() {}`!
+catch (FileNotFoundException) { Console.WriteLine("Please make a `password.txt`!"); }
